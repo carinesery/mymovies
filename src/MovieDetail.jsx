b4572f5081b45bb59682bda725fa6c3b92ec5f40
Carine Sery <carine.sery@gmail.com>
2026-01-09 14:27:46 +0100

    Ajout des films similaires dans MovieDetail

--- src/MovieDetail.jsx
+++ src/MovieDetail.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect, useContext } from "react";
-import {useParams} from "react-router-dom";
-import {WishlistContext} from "./WishlistProvider"
+import { useParams } from "react-router-dom";
+import { WishlistContext } from "./WishlistProvider"
 
 function MovieDetail() {
-    const {addToWishlist, removeFromWishlist} = useContext(WishlistContext);
+    const { addToWishlist, removeFromWishlist } = useContext(WishlistContext);
     const [movie, setMovie] = useState({});
     const [actors, setActors] = useState([]);
-    const {id} = useParams();
+    const [similarFilms, setSimilarFilms] = useState([]);
+    const { id } = useParams();
 
     const apiKey = import.meta.env.VITE_APP_TMDB_KEY
 
@@ -24,6 +25,13 @@ function MovieDetail() {
                 console.log(data.cast);
                 setActors(data.cast.slice(0, 10))
             })
+
+        fetch(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setSimilarFilms(data.results)
+            })
     }, [id]);
 
 
@@ -35,8 +43,16 @@ function MovieDetail() {
             <li>{movie.vote_average}</li>
             <li>{movie.overview}</li>
             <li>
+                <h2>Acteurs : </h2>
                 <ul>{actors.map((actor, index) => (
                     <li key={index}>{actor.name}</li>
+                ))}
+                </ul>
+            </li>
+            <li>
+                <h2>Films similaires : </h2>
+                <ul>{similarFilms.map((similarFilm, index) => (
+                    <li key={index}>{similarFilm.title}</li>
                 ))}
                 </ul>
             </li>
