82ecf9d81cdb6fd3826d996470f0458ee0dc8454
Carine Sery <carine.sery@gmail.com>
2026-01-09 15:15:52 +0100

    Ajout des styles avec module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import {WishlistProvider} from "./WishlistProvider.jsx";
-import Navbar from "./Navbar.jsx"
-import MovieList from './MovieList.jsx'
-import MovieDetail from './MovieDetail.jsx'
-import Wishlist from './Wishlist.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { WishlistProvider } from "./WishlistProvider.jsx";
+import Navbar from "./Navbar.jsx";
+import MovieList from './MovieList.jsx';
+import MovieDetail from './MovieDetail.jsx';
+import Wishlist from './Wishlist.jsx';
+import styles from "./App.module.css";
 
 
 function App() {
 
   return (
-    <>
+    <div className={styles.appContainer}>
       <WishlistProvider>
         <BrowserRouter>
           <Navbar />
+          <div className={styles.content}>
+            <Routes>
+              <Route path="/" element={<MovieList />}></Route>
+              <Route path="/movie/:id" element={<MovieDetail />}></Route>
+              <Route path="/wishlist" element={<Wishlist />}></Route>
+            </Routes>
 
-          <Routes>
-            <Route path="/" element={<MovieList />}></Route>
-            <Route path="/movie/:id" element={<MovieDetail />}></Route>
-            <Route path="/wishlist" element={<Wishlist />}></Route>
-          </Routes>
+          </div>
 
         </BrowserRouter>
       </WishlistProvider>
-    </>
+    </div>
   )
 }
 
--- src/MovieDetail.jsx
+++ src/MovieDetail.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
-import { WishlistContext } from "./WishlistProvider"
+import { WishlistContext } from "./WishlistProvider";
+import styles from "./MovieDetail.module.css";
 
 function MovieDetail() {
     const { addToWishlist, removeFromWishlist } = useContext(WishlistContext);
@@ -36,32 +37,76 @@ function MovieDetail() {
 
 
     return (
-        <ul>
-            <li><img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} /></li>
-            <li>{movie.title}</li>
-            <li>{movie.release_date}</li>
-            <li>{movie.vote_average}</li>
-            <li>{movie.overview}</li>
-            <li>
-                <h2>Acteurs : </h2>
-                <ul>{actors.map((actor, index) => (
-                    <li key={index}>{actor.name}</li>
-                ))}
-                </ul>
-            </li>
-            <li>
-                <h2>Films similaires : </h2>
-                <ul>{similarFilms.map((similarFilm, index) => (
-                    <li key={index}>{similarFilm.title}</li>
-                ))}
-                </ul>
-            </li>
-            <li>
-                <button onClick={() => addToWishlist(movie)}>Ajouter à ma wihslist</button> {/** Ajout à la wihslist */}
-                <button onClick={() => removeFromWishlist(movie.id)}>Retirer de ma wihslist</button> {/** Suppression à la wihslist */}
-            </li>
+        <div className={styles.container}>
+           
+            <div className={styles.header}>
+                <img
+                    className={styles.poster}
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                />
 
-        </ul>
+                <div className={styles.info}>
+                    <h1>{movie.title}</h1>
+                    <p>Date de sortie : {movie.release_date}</p>
+                    <p>Note : ⭐ {movie.vote_average}</p>
+                    <p>{movie.overview}</p>
+
+                    <div className={styles.buttons}>
+                        <button onClick={() => addToWishlist(movie)}>
+                            Ajouter à ma wishlist
+                        </button>
+                        <button onClick={() => removeFromWishlist(movie.id)}>
+                            Retirer de ma wishlist
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.section}>
+                <h2>Acteurs</h2>
+                <ul className={styles.actors}>
+                    {actors.map((actor) => (
+                        <li key={actor.id}>{actor.name}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className={styles.section}>
+                <h2>Films similaires</h2>
+                <ul className={styles.similar}>
+                    {similarFilms.map((film) => (
+                        <li key={film.id}>{film.title}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+        // <ul>
+        //     <li><img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} /></li>
+        //     <li>{movie.title}</li>
+        //     <li>{movie.release_date}</li>
+        //     <li>{movie.vote_average}</li>
+        //     <li>{movie.overview}</li>
+        //     <li>
+        //         <h2>Acteurs : </h2>
+        //         <ul>{actors.map((actor, index) => (
+        //             <li key={index}>{actor.name}</li>
+        //         ))}
+        //         </ul>
+        //     </li>
+        //     <li>
+        //         <h2>Films similaires : </h2>
+        //         <ul>{similarFilms.map((similarFilm, index) => (
+        //             <li key={index}>{similarFilm.title}</li>
+        //         ))}
+        //         </ul>
+        //     </li>
+        //     <li>
+        //         <button onClick={() => addToWishlist(movie)}>Ajouter à ma wihslist</button> {/** Ajout à la wihslist */}
+        //         <button onClick={() => removeFromWishlist(movie.id)}>Retirer de ma wihslist</button> {/** Suppression à la wihslist */}
+        //     </li>
+
+        // </ul>
     )
 
 }
