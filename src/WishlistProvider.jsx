81dcbbf0b95d74cc94cce6b90e8e214e0984a804
Carine Sery <carine.sery@gmail.com>
2026-01-09 14:04:51 +0100

    Création du localStorage

--- src/WishlistProvider.jsx
+++ src/WishlistProvider.jsx
@@ -1,15 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const WishlistContext = createContext();
 
 export function WishlistProvider({ children }) {
-  const [wishlist, setWishlist] = useState([]);
+  const [wishlist, setWishlist] = useState(() => {
+    const wishlistSaved = localStorage.getItem("wishlist");
+    return wishlistSaved ? JSON.parse(wishlistSaved) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+  }, [wishlist]);
 
   const addToWishlist = (movie) => {
     if (!wishlist.some((item) => item.id === movie.id)) {
       setWishlist([...wishlist, movie]);
     }
   };
+
 
   const removeFromWishlist = (movieId) => {
     setWishlist(wishlist.filter((item) => item.id !== movieId));
