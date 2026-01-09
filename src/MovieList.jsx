167c661e9bc8cce067f1f7b163f8784e3166a580
Carine Sery <carine.sery@gmail.com>
2026-01-09 14:47:21 +0100

    Ajout de la pagination dans MovieList

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,23 +1,29 @@
 import { useState, useEffect } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function MovieList() {
     const [moviesList, setMoviesList] = useState([]);
     const [category, setCategory] = useState("popular");
     const [search, setSearch] = useState("");
+    const [page, setPage] = useState(1);
 
 
     const apiKey = import.meta.env.VITE_APP_TMDB_KEY
+
     useEffect(() => {
-        const url = search 
-        ? `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${search}`
-        : `https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`
+        setPage(1);
+    }, [category, search]);
+
+    useEffect(() => {
+        const url = search
+            ? `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${search}&page=${page}`
+            : `https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}&page=${page}`
         fetch(url)
             .then((response) => response.json())
             .then((data) => {
                 setMoviesList(data.results);
             });
-    }, [category, search]);
+    }, [category, search, page]);
 
     const filteredMovies = moviesList.filter((movie) => movie.title.toLowerCase().includes(search.toLowerCase()));
 
@@ -32,10 +38,15 @@ function MovieList() {
                 />
             </div>
             <div>
-                <button onClick={() => setCategory("popular")}>Populaires</button>
-                <button onClick={() => setCategory("now_playing")}>A l'affiche</button>
-                <button onClick={() => setCategory("top_rated")}>Les mieux notés</button>
-                <button onClick={() => setCategory("upcoming")}>A venir</button>
+                <button onClick={() => { setCategory("popular"); setPage(1) }}>Populaires</button>
+                <button onClick={() => { setCategory("now_playing"); setPage(1) }}>A l'affiche</button>
+                <button onClick={() => { setCategory("top_rated"); setPage(1) }}>Les mieux notés</button>
+                <button onClick={() => { setCategory("upcoming"); setPage(1) }}>A venir</button>
+            </div>
+            <div>
+                <button onClick={() => setPage(page - 1)} disabled={page === 1}>Page précédente</button>
+                <span>Page {page}</span>
+                <button onClick={() => setPage(page + 1)}>Page suivante</button>
             </div>
             <ul>{filteredMovies.map((movie, index) => (
                 <li key={index}>
